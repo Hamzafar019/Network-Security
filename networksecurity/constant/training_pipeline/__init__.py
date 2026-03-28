18b6f069b3708547164725689d205677b0c43888
Hamzafar019 <mhz15019@gmail.com>
2026-03-28 16:43:25 +0500

    s3 bucket and github action

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -72,3 +72,6 @@ MODEL_TRAINER_OVER_FIITING_UNDER_FITTING_THRESHOLD: float = 0.05
 
 SAVED_MODEL_DIR =os.path.join("saved_models")
 MODEL_FILE_NAME = "model.pkl"
+
+
+TRAINING_BUCKET_NAME = "netwworksecurity"
